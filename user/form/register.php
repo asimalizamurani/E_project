4a99069a83c7bbf0d43d557b0dc0b6ae40c29a19
Asim Ali <asimalizamurani@gmail.com>
2024-07-29 17:29:37 +0500

    I made registration form login form cart page and category pages as well

--- user/form/register.php
+++ user/form/register.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Form</title>
-    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="../css/form.css">
+    <link rel="stylesheet" href="../css/responsive.css">
 </head>
 <body>
   <div class="wrapper">
